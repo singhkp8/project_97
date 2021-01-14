eb8a96d6506d581ce24c5d80573ccc008c0e6148
Kavya Pal Singh <65481230+singhkp8@users.noreply.github.com>
2021-01-14 11:56:29 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,13 @@
+import random
+
+Guess = random.randint(0,10)
+print(Guess)
+
+userGuess = int(input("Enter Your Guess :- \n"))
+
+if (Guess == userGuess):
+    print("You Won")
+    print("Your Guess is :- " + userGuess)
+    print(Guess)
+elif(Guess != userGuess):
+    print("You Lose")
